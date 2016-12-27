f686380c7f45016c58b26a45f8d47e59965fce2c
=Fred Gravil <fgravil18@knights.ucf.edu>
2016-12-26 22:57:50 -0500

    added timer and countdown pages

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,8 @@ var ReactDOM = require('react-dom');
 {/*ES5 desctructuring syntax */}
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 
 //Load foundations-sites
 require('style!css!foundation-sites/dist/css/foundation.min.css');
@@ -15,7 +17,8 @@ require('style!css!sass!applicationStyles');
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
-    
+      <IndexRoute component={Timer} />
+      <Route path="countdown" component={Countdown} />
     </Route>
   </Router>,
     document.getElementById('app')
