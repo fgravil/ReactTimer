edfb512995a62c185f8abd3cf398e7b374e737a4
=Fred Gravil <fgravil18@knights.ucf.edu>
2016-12-28 02:23:10 -0500

    Add CountdownForm and tests

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     ],
     output: {
         path: __dirname,
-        filename: './wwwroot/js/site.js'
+        filename: './wwwroot/js/bundle.js'
     },
     resolve: {
         root: __dirname,
@@ -26,6 +26,7 @@ module.exports = {
             Nav: 'app/components/Nav.jsx',
             Timer: 'app/components/Timer.jsx',
             Countdown: 'app/components/Countdown.jsx',
+            CountdownForm: 'app/components/CountdownForm.jsx',
             Clock: 'app/components/Clock.jsx',
             applicationStyles: 'app/styles/app.scss'
         },
